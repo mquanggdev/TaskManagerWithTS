36550940dfd46893bf0c0fab5b51a3a371dbde48
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-13 18:18:25 +0700

    tải về cors

--- index.ts
+++ index.ts
@@ -5,11 +5,24 @@ import { connectDatabase } from "./config/database";
 import Task from "./models/task.model";
 import { routesApi } from "./routes/client/index.route";
 import bodyParser from "body-parser";
+import cors from "cors";
 connectDatabase();
 
 
 const app:Express = express();
 const port: number | string = process.env.PORT || 3000;
+// CORS
+// Cách 1: Tất cả tên miền được phép truy cập
+app.use(cors());
+
+// Cách 2: Áp dụng cho 1 tên miền cụ thể
+// const corsOptions = {
+//   origin: 'https://abc.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+// End CORS
+
 // parse application/json
 app.use(bodyParser.json());
 
