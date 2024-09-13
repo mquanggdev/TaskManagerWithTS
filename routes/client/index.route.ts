163253e4682a0647a6c43597bc4d6c7014c3b65f
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-13 18:26:00 +0700

    Đăng ký đăng nhập và profile

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,6 +1,9 @@
 import { Express } from "express";
 import { taskRoute } from "./task.route";
+import { userRoute } from "./user.route";
+import { requireAuth } from "../../middlewares/client/auth.middleware";
 
 export const routesApi = (app: Express) => {
-  app.use("/tasks", taskRoute);
+  app.use("/tasks", requireAuth, taskRoute);
+  app.use("/users", userRoute);
 }
---
+++ routes/client/user.route.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+
+import * as controller from "../../controllers/client/user.controller";
+import { requireAuth } from "../../middlewares/client/auth.middleware";
+
+router.post("/register", controller.register);
+router.post("/login", controller.login);
+router.get("/profile", requireAuth, controller.profile);
+
+export const userRoute = router;
