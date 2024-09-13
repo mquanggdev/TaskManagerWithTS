026e008a0cae3d6f8c33d61ec44161035799988d
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-13 18:16:37 +0700

    Tính năng chỉnh sửa công việc

--- routes/client/task.route.ts
+++ routes/client/task.route.ts
@@ -8,5 +8,6 @@ router.get("/", controller.index);
 router.get("/detail/:id", controller.detail);
 router.patch("/change-status", controller.changeStatus);
 router.post("/create", controller.create);
+router.patch("/edit/:id", controller.edit);
 
 export const taskRoute = router;
